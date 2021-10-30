c177efda40cbb5fc5111c9f47144b8dd9c2054f1
fg <fg@superman.com>
2021-10-30 22:29:10 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitDemoApplication.java
+++ src/main/java/com/atguigu/git/GitDemoApplication.java
@@ -7,6 +7,7 @@ public class GitDemoApplication {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4 by hot-fix");
 
     }
 }
