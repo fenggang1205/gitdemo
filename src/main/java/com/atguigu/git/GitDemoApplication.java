684f2fd7da7d45c40dbcc4ea0a6406f9edfb783a
fenggang1205 <53340390+fenggang1205@users.noreply.github.com>
2021-10-30 22:57:12 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitDemoApplication.java
+++ src/main/java/com/atguigu/git/GitDemoApplication.java
@@ -11,6 +11,7 @@ public class GitDemoApplication {
         System.out.println("master  test ");
         System.out.println("hot-fix  test");
         System.out.println("push  test");
+        System.out.println("pull  test");
 
     }
 }
