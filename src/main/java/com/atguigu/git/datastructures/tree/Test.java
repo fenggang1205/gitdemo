ecf35752c5df16204d541d1c049ae05aa2631dd5
FG <1479843655@qq.com>
2022-02-24 23:14:37 +0800

    数据结构与算法

---
+++ src/main/java/com/atguigu/git/datastructures/tree/Test.java
@@ -0,0 +1,9 @@
+package com.atguigu.git.datastructures.tree;
+
+public class Test {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/main/java/com/atguigu/git/nowcoder/ListNodeReverse.java
@@ -0,0 +1,34 @@
+package com.atguigu.git.nowcoder;
+
+import java.util.Stack;
+
+public class ListNodeReverse {
+
+    public static void main(String[] args) {
+        /**
+         * 如当输入链表{1,2,3}时，
+         经反转后，原链表变为{3,2,1}，所以对应的输出为{3,2,1}。
+         */
+
+    }
+
+    private ListNode reverseList(ListNode head){
+
+
+
+        return null;
+    }
+
+
+}
+
+class ListNode {
+
+    int val;
+
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
